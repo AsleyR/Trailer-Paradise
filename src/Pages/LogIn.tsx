90645202982dce0b0f4a1d08e3cccd86e9178c79
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-07-28 15:57:27 -0600

    Add Login and Sign Up Tabs

--- src/Pages/LogIn.tsx
+++ src/Pages/LogIn.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import LoginForm from '../components/logIn/LoginForm'
+
+interface IState {
+  login: {
+    user: string
+    pass: string
+  }
+}
 
 const LogIn = () => {
+  const [login, setLogin] = useState<IState["login"]>(
+    {
+      user: "",
+      pass: ""
+  })
+
   return (
     <div>
-      <h1>This is the Log-in page!</h1>
+      <h2>Sign in into your account</h2>
+      <LoginForm />
+      <div>
+        <p>Don't have an account? <Link to={"/sign-up"}>Create one today!</Link></p>
+      </div>
     </div>
   )
 }
--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SignUpForm from '../components/signUp/SignUpForm'
+import { Link } from "react-router-dom"
+
+
+interface IState {
+  signUp: {
+    user: string
+    pass: string
+  }
+}
 
 const SignUp = () => {
+  const [signUp, setSignUp] = useState<IState["signUp"]>({
+      user: "",
+      pass: ""
+  })
+
+
   return (
     <div>
-      <h1>This is the Sign-up page!</h1>
+      <h2>Create an account</h2>
+      <SignUpForm />
+      <p>Already have an account? <Link to={"/log-in"}>Sign in</Link></p>
     </div>
   )
 }
