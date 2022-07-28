b3632382ed069107d01b64658ff7ef44d56ad194
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-07-28 15:53:40 -0600

    Add Gallery tab

--- src/Pages/Gallery.tsx
+++ src/Pages/Gallery.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import TrailerCard from '../components/TrailerCard'
 
 const Gallery = () => {
   return (
     <div>
-        <h1>This is the Gallery page!</h1>
+        <h2>Lastest Movie Trailers</h2>
+        <TrailerCard />
     </div>
   )
 }
