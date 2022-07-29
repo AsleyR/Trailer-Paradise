3475a37f5f2e89a86d04971c52a1a7321ca4cb75
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-07-28 18:02:51 -0600

    Change Header logo to a Link component that goes to '/'

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from './Navbar'
 
 const Header = () => {
   return (
     <header className='header'>
-        <h1>Trailer Paradise</h1>
+      <Link className='header-link' to={"/"}><h1>Trailer Paradise</h1></Link>
         <Navbar />
     </header>
   )
