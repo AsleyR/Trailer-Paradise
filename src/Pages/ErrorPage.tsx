4323d71c76df4681c3cac32ea06041957bec0222
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-09 17:58:37 -0600

    Edit Error Page

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ErrorPage = () => {
   return (
-    <div>
-        <h1>ERROR 404</h1>
-        <h2>The Page You Were Looking For Doesn't Exist!</h2>
+    <div className='grid justify-items-center p-20 my-28'>
+        <h1 className='font-bold text-6xl sm:text-5xl md:text-6xl'>ERROR 404</h1>
+        <h2 className='my-5 text-xl sm:text-lg md:text-xl'>The Page You Were Looking For Doesn't Exist!</h2>
     </div>
   )
 }
