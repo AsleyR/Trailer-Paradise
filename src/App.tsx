611dd0c933d989a86c39482a108a431067861b82
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-09 18:00:49 -0600

    Add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,10 @@ import ErrorPage from './pages/ErrorPage';
 import Gallery from './pages/Gallery';
 import Home from './pages/Home';
 import LogIn from './pages/Login';
+import Profile from './pages/Profile';
 import Search from './pages/Search';
 import SignUp from './pages/SignUp';
+import Trailer from './pages/Trailer';
 
 function App() {
   return (
@@ -19,10 +21,12 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path='/search' element={<Search/>}/>
+        <Route path='/trailer' element={<Search/>}/>
+        <Route path='/trailer/:trailerId' element={<Trailer />}/>
         <Route path="/gallery" element={<Gallery />}/>
         <Route path="/log-in" element={<LogIn />}/>
         <Route path="/sign-up" element={<SignUp />}/>
+        <Route path="/profile/:userId" element={<Profile />}/>
         <Route path="*" element={<ErrorPage />}/>
       </Routes>
     </Router>
