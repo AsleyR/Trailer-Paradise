74dfc1c4f4037d82e0c43a6027794abec567c9a0
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-09 18:00:11 -0600

    Finish Sign Up front-edn

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -18,10 +18,15 @@ const SignUp = () => {
 
 
   return (
-    <div>
-      <h2>Create an account</h2>
-      <SignUpForm />
-      <p>Already have an account? <Link to={"/log-in"}>Sign in</Link></p>
+    <div className='grid grid-cols-none md:grid-cols-2 gap-20 px-20 py-10'>
+      <div>
+        <h2 className='text-2xl sm:text-2xl'>Create an account today and enjoy all the latest movie and serie trailers!</h2>
+        <SignUpForm />
+        <p className='text-lg sm:text-xl md:text-base'>Already have an account? <Link className='hover:text-col2 transition-all' to={"/log-in"}>Log in</Link></p>
+      </div>
+      <div className="bg-none bg-fixed bg-no-repeat bg-cover bg-center rounded md:bg-signup lg:bg-signup">
+        {/* <img className='max-w-full min-h-auto rounded' src='movieNetflix.jpg' alt='Computer with Netflix Open in Browser'></img> */}
+      </div>
     </div>
   )
 }
--- src/components/signUp/SignUpForm.tsx
+++ src/components/signUp/SignUpForm.tsx
@@ -25,44 +25,47 @@ const SignUpForm: React.FC = () => {
   const passLengthCheck = () => {
     if (input.pass.length < 8 && input.pass.length != 0) {
       return (
-        <label>Password is too short!</label>
+        <label className='mx-5 text-rose-600'>Password is too short!</label>
       )
     }
   }
 
   return (
-    <div>
-      <p>Email</p>
+    <div className='my-6'>
+      <p className='pt-3 text-xl'>Email</p>
       <input
         type="email"
         name="email"
-        placeholder='Email'
+        placeholder=''
         value={input.email}
         onChange={handleChange}
+        className="border border-slate-400 rounded w-full md:w-64"
       />
       <br></br>
-      <p>Username</p>
+      <p className='pt-3 text-xl'>Username</p>
       <input
         type="text"
         name="user"
-        placeholder='Username'
+        placeholder=''
         value={input.user}
         onChange={handleChange}
+        className="border border-slate-400 rounded w-full md:w-64"
       />
       <br></br>
-      <p>Password</p>
+      <p className='pt-3 text-xl'>Password</p>
       <div>
         <input
           type="password"
           name="pass"
-          placeholder='Password'
+          placeholder=''
           value={input.pass}
           onChange={handleChange}
+          className="border border-slate-400 rounded w-full md:w-64"
         />
         {passLengthCheck()}
       </div>
       <br></br>
-      <button>Create Account</button>
+      <button className='w-full md:w-auto bg-col1 hover:bg-col2 transition-all text-white px-3 py-1 rounded'>Create Account</button>
     </div>
   )
 }
