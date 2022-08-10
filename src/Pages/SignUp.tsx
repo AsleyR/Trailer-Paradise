c28b9acd8ab4a8bb4155607775a2ce26a1f20715
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-09 19:57:25 -0600

    Small change to sign up already have an account message

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -22,7 +22,7 @@ const SignUp = () => {
       <div>
         <h2 className='text-2xl sm:text-2xl'>Create an account today and enjoy all the latest movie and serie trailers!</h2>
         <SignUpForm />
-        <p className='text-lg sm:text-xl md:text-base'>Already have an account? <Link className='hover:text-col2 transition-all' to={"/log-in"}>Log in</Link></p>
+        <p className='text-lg sm:text-xl md:text-base'>Already have an account? <Link className='hover:text-col2 transition-all' to={"/log-in"}>Log in instead!</Link></p>
       </div>
       <div className="bg-none bg-fixed bg-no-repeat bg-cover bg-center rounded md:bg-signup lg:bg-signup">
         {/* <img className='max-w-full min-h-auto rounded' src='movieNetflix.jpg' alt='Computer with Netflix Open in Browser'></img> */}
