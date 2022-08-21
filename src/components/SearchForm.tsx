73e99047d23e4cbccdc24050eb437894a924fce9
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-21 10:02:37 -0600

    Change style of SearchForm component

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 interface IState {
     search: {
@@ -19,15 +20,16 @@ const SearchForm: React.FC = () => {
   }
 
   return (
-    <div>
+    <div className='grid gap-3 md:gap-0 md:flex pt-2'>
       <input
+        className='w-full md:w-full lg:w-1/2 bg-gray1 placeholder:text-gray3 focus:outline-none focus:bg-gray2 px-2 py-2 md:py-3 rounded md:rounded-none md:rounded-l transition-all'
         type="text"
-        placeholder="search..."
+        placeholder="Name of movie or series.."
         value={input.search}
         name="search"
         onChange={handleChange}
-      />
-      <button>Search</button>
+      ></input>
+      <button className='flex py-2 md:py-0 bg-col1 hover:bg-col2 md:hover:bg-col2 md:bg-col1 rounded md:rounded-none md:rounded-r justify-center text-white text-white items-center md:px-3 transition-all'><FaSearch /></button>
     </div>
   )
 }
