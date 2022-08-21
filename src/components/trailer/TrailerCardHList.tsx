5f2bca16d310cbb51c58ea1e26af6a7435da9ae0
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-21 10:04:32 -0600

    Add TrailerCardHList component

---
+++ src/components/trailer/TrailerCardHList.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import TrailerCard from './TrailerCard'
+
+interface IProps {
+  trailerList: {
+    url: string;
+    imageUrl: string;
+    trailerName: string;
+  }[]
+}
+
+const TrailerCardHList = ({ trailerList }: IProps) => {
+    const navigation = useNavigate()
+  const [trailer, setTrailer] = useState(trailerList)
+
+  const renderTrailerCard = () => {
+    return (
+        trailer.map((trailer) => {
+            return (
+              <TrailerCard trailer={trailer}/>
+            )
+        })
+    )
+  }
+  
+    return (
+    <ul className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-x-5 sm:gap-x-5 md:gap-x-5 gap-y-5'>
+    {renderTrailerCard()}
+    </ul>
+  )
+}
+
+
+export default TrailerCardHList
