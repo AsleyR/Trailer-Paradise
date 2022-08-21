7120716b486fa8c3212e8fa0aca3f9a4732ceba1
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-21 09:58:59 -0600

    Change Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,22 @@ module.exports = {
         'signup': "url('../public/images/movieNetflix.jpg')",
         'forest': "url('../public/images/forestBg.jpg')",
         'moviecinema': "url('../public/images/movieCinema.jpg')",
+        'moviecinema2': "url('../public/images/movieCinema2.jpg')",
+        'moviecinema3': "url('../public/images/movieCinema3.jpg')"
       },
       colors: {
       'bg1': "#2b2d42",
       'bg2': "#8d99ae",
-      'col1': "#d80032",
-      'col2': "#ef233c",
+      'col1': "#BD002C",
+      'col2': "#D80032",
       'extracol': "edf2f4",
-      'dark1': "#020412"
-    },
+      'dark1': "#060606",
+      'dark2': "#181818",
+      'dark3': "#0F0F0F",
+      'gray1': "#404040",
+      'gray2': "#525252",
+      'gray3': "#8C8C8C"
+      },
     },
   },
   plugins: [],
