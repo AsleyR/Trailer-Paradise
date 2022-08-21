433e7efc3d33a499839fc36d8affeb8f8843eb5b
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-21 10:04:46 -0600

    Add TrailerVideo component

---
+++ src/components/trailer/TrailerVideo.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface IProps {
+    trailer: {
+        trailerUrl: string
+    }
+}
+
+const TrailerVideo = ({trailerUrl}: IProps['trailer']) => {
+  return (
+    <div className='relative pb-[56.25%] h-0 transition-all'>
+      <iframe
+      src={`${trailerUrl}`} title="YouTube video player" 
+      frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowFullScreen className='absolute outline-0 focus:outline-0 focus-visible:outline-none top-0 left-0 w-[100%] h-[100%] rounded-none md:rounded justify-self-center transition-all'>
+      </iframe>
+    </div>
+  )
+}
+
+export default TrailerVideo
