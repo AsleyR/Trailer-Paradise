153542a286719706e9fa781cd4d66b6c00b39705
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-21 19:23:47 -0600

    Modify component to consume custom Trailer Paradise API

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
 import SearchForm from '../components/SearchForm'
 import TrailerCard from '../components/trailer/TrailerCardHList'
 import { useLocation } from 'react-router-dom'
 
 interface IState {
   trailerList: {
+    id: number;
     url: string
     imageUrl: string
     trailerName: string
@@ -13,63 +15,32 @@ interface IState {
 
 const Search = () => {
   const location = useLocation()
-  const [trailer, setTrailer] = useState<IState['trailerList']>([
-    {
-        url: "/trailer/123",
-        imageUrl: "https://cdn.themovieseries.net/cover/minions-the-rise-of-gru.png",
-        trailerName: "Minions: The Rise of Gru"
-    },
-    {
-        url: "/trailer/123",
-        imageUrl: "https://cdn.themovieseries.net//titanic-jxq/cover.png",
-        trailerName: "Titanic"
-    },
-    {
-      url: "/trailer/123",
-      imageUrl: "https://cdn.themovieseries.net/cover/fight-club.png",
-      trailerName: "Fight Club"
-    },
-    {
-      url: "/trailer/123",
-      imageUrl: "https://cdn.themovieseries.net/cover/spider-man-far-from-home.png",
-      trailerName: "Spider-Man: Far from Home"
-    },
-    {
-      url: "/trailer/peaky-blinders-season-3",
-      imageUrl: "https://cdn.themovieseries.net//peaky-blinders-season-3-jsv/cover.png",
-      trailerName: "Peaky Blinders - Season 3"
-    },
-    {
-      url: "/trailer/the-batman",
-      imageUrl: "https://cdn.themovieseries.net/cover/the-batman.png",
-      trailerName: "The Batman"
-    },
-    {
-      url: "/trailer/breaking-bad-season-1",
-      imageUrl: "https://cdn.themovieseries.net//breaking-bad-season-1-hsh/cover.png",
-      trailerName: "Breaking Bad - Season 1"
-    },
-    {
-      url: "/trailer/the-queens-gambit-season-1",
-      imageUrl: "https://cdn.themovieseries.net/cover/the-queens-gambit-season-1.png",
-      trailerName: "The Queen's Gambit - Season 1"
-    },
-    {
-      url: "/trailer/once-upon-a-time-in-hollywood",
-      imageUrl: "https://cdn.themovieseries.net/cover/once-upon-a-time-in-hollywood.png",
-      trailerName: "Once Upon a Time in Hollywood"
+  const [trailer, setTrailer] = useState<IState['trailerList']>([])
+  const [hasLoaded, setHasLoaded] = useState<Boolean>();
+  const [hasSearch, setHasSearch] = useState<Boolean>();
+
+  useEffect(() => {
+    const getTrailerList = () => {
+      Axios.get("http://localhost:4000/api/trailers").then(res => {
+        setTrailer(res.data)
+        setHasLoaded(true) // Prevents components from rendering before useEffect finishes
+      }).catch(err => {
+        console.log(err)
+      })
     }
-  ])
+
+    getTrailerList()
+  }, [])
 
   return (
     <div className='max-w-full h-full bg-dark2'>
       <div className='pt-[5rem] md:pt-[10rem]'>
         <div className='grid px-5 gap-10 md:px-5 lg:px-20 py-10 text-white'>
           <div>
-            <h2 className='font-bold text-2xl md:text-4xl'>Search results for "{location.pathname}"</h2>
+            {hasSearch ? <h2 className='font-bold text-2xl md:text-4xl'>Search results for "{location.pathname}"</h2>: <h2 className='font-bold text-2xl md:text-4xl'>Search</h2>}
             <SearchForm />
           </div>
-          <TrailerCard trailerList={trailer}/>
+            {hasLoaded ? <TrailerCard trailerList={trailer}/>: null}
         </div>
       </div>
     </div>
--- client/src/pages/Trailer.tsx
+++ client/src/pages/Trailer.tsx
@@ -1,37 +1,52 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import { FaListUl, FaShare } from 'react-icons/fa'
+import Axios from 'axios'
 import TrailerVideo from '../components/trailer/TrailerVideo'
 import TrailerCard from '../components/trailer/TrailerCard'
 
 interface IState {
   trailer: {
-    trailerUrl: string;
-    title: string;
-    date: string; // Temp
-    description: string;
+    id: number;
+    trailerName: string;
+    trailerDescription: string;
+    videoUrl: string;
+    videoReleaseDate: string;
   }
 }
 
 const Trailer = () => {
   let { trailerId } = useParams()
-  const [trailer, setTrailer] = useState<IState['trailer']>(
-    {
-      trailerUrl: "https://youtube.com/embed/JwXfVGjSz-4",
-      title: "Peaky Blinders - Season 1",
-      date: "Jul 30, 2015",
-      description: "A gangster family epic set in 1900s England, centering on a gang who sew razor blades in the peaks of their caps, and their fierce boss Tommy Shelby."
+  const navigation = useNavigate()
+
+  const [trailer, setTrailer] = useState<IState['trailer']>()
+  const [hasLoaded, setHasLoaded] = useState<Boolean>()
+
+  useEffect(() => {
+    const getTrailerData = () => {
+      Axios.get(`http://localhost:4000/api/trailers/${trailerId}`).then(res => {
+        if (res.status = 200) {
+          setTrailer(res.data)
+          setHasLoaded(true)
+        }
+
+      }).catch(err => {
+        console.log(err)
+        navigation('/error-404')
+      })
     }
-  )
+
+    getTrailerData()
+  }, [])
 
   const renderTrailerData = () => {
     return (
       <div className='justify-self-center w-auto px-5 md:pl-[2rem] lg:pl-[5rem] py-3 text-white transition-all'>
         <div className='justify-self-center'>
-          <h1 className='text-2xl md:text-2l font-bold'>{trailer.title}</h1>
-          <p className='font-medium'>{trailer.date}</p>
+          <h1 className='text-2xl md:text-2l font-bold'>{trailer?.trailerName}</h1>
+          <p className='font-medium'>{trailer?.videoReleaseDate}</p>
           <div className='w-full'>
-            <p className='pt-2 md:pt-1 md:text-base'>{trailer.description}</p>
+            <p className='pt-2 md:pt-1 md:text-base'>{trailer?.trailerDescription}</p>
           </div>
         </div>
         <div className='mt-3'>
@@ -49,7 +64,7 @@ const Trailer = () => {
       <div className='bg-dark1 border-b-2 border-gray1 grid grid-cols-1 md:grid-cols-none py-0 md:py-12 justify-center shadow'>
         <div className='justify-center px-0 md:px-0 w-auto md:w-[45rem] lg:w-[50rem] transition-all'>
           <div className='grid grid-cols-1 justify-center max-w-[55rem]'>
-            <TrailerVideo trailerUrl={trailer.trailerUrl}/>
+            {hasLoaded ? <TrailerVideo videoUrl={trailer?.videoUrl}/>: null}
           </div>
         </div>
       </div>
@@ -66,7 +81,7 @@ const Trailer = () => {
             <div className=''>
               <h1 className='text-white text-center text-2xl font-bold'>Related Content</h1>
               <div>
-                <TrailerCard trailer={{url: "/trailer/Breaking-Bad", imageUrl: "https://cdn.themovieseries.net//breaking-bad-season-1-hsh/cover.png", trailerName: "Breaking Bad - Season 1"}}/>
+                <TrailerCard trailer={{id: 7, url: 	"/trailer/breaking-bad-season-1", imageUrl: "https://cdn.themovieseries.net//breaking-bad-season-1-hsh/cover.png", trailerName: "Breaking Bad - Season 1"}}/>
               </div>
             </div>
           </div>
