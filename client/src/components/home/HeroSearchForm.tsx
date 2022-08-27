d57694e8085f2a52021a8ad1b06f2260a4b246ae
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-27 17:56:56 -0600

    Small change in style

--- client/src/components/home/HeroSearchForm.tsx
+++ client/src/components/home/HeroSearchForm.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 const HeroSearchForm = () => {
   return (
     <div className='grid grid-rows-auto gap-3 sm:gap-0 sm:flex sm:justify-center mt-10'>
         <input 
             placeholder='Name of movie or series...'
-            className='px-3 py-5 w-full sm:w-8/12 rounded sm:rounded-none sm:rounded-l text-lg text-white placeholder:text-gray3 bg-dark2 focus:bg-gray1 focus:outline-none' 
+            className='px-3 py-5 w-full sm:w-8/12 rounded sm:rounded-none sm:rounded-l text-lg text-white placeholder:text-gray3 bg-dark2 focus:bg-dark3 focus:outline-none' 
             type="text"
             name='searchForm'
         />
-        <button className='w-full sm:w-auto justify-self-center sm:justify-self-auto text-white bg-col1 hover:bg-col2 transition-all text-xl sm:text-2xl sm:px-10 py-2 sm:py-3 rounded sm:rounded-none sm:rounded-r'>Search</button>
+        <button className='w-full grid justify-center sm:w-auto text-white bg-col1 hover:bg-col2 items-center transition-all text-xl sm:text-2xl sm:px-5 py-3 rounded sm:rounded-none sm:rounded-r'><FaSearch/></button>
     </div>
   )
 }
