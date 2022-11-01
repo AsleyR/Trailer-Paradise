dab1dd9c99baa162dbc36e9083c4fe1a922012d6
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-10-31 21:00:30 -0600

    Update TPFeatures component

--- client/src/components/home/TPFeatures.tsx
+++ client/src/components/home/TPFeatures.tsx
@@ -5,8 +5,8 @@ const TPFeatures = () => {
     <div className='bg-dark1'>
         <div className='grid gap-10 md:gap-0 grid-rows-2 lg:grid-rows-none lg:grid-cols-2 border-t-8 border-dark2 items-center px-10 sm:px-40 md:px-40 py-24'>
             <div className=''>
-                <h1 className='text-white text-center lg:text-left text-4xl sm:text-4xl md:text-5xl font-bold'>Save, and Watch it later.</h1>
-                <p className='text-white text-center lg:text-left text-2xl sm:text-2xl pt-5'>With a Trailer Paradise account, you can choose to watch any of your favorite trailers at any time!</p>
+                <h1 className='text-white text-center lg:text-left text-4xl sm:text-4xl md:text-5xl font-bold'>Watch what you want.</h1>
+                <p className='text-white text-center lg:text-left text-2xl sm:text-2xl pt-5'>With our great selection of titles, there is lots of content to enjoy either with friends or alone.</p>
             </div>
             <div className='flex justify-center lg:justify-end'>
                 <img className='text-slate-300 w-8/12 h-full rounded' src="images/watching-tv.jpg" alt='TV with FRIENDS on Netflix'></img>
@@ -17,7 +17,7 @@ const TPFeatures = () => {
                 <img className='text-slate-300 w-8/12 h-full rounded' src='images/devices.jpg' alt='person on the computer holding a phone'></img>
             </div>
             <div className=''>
-                <h1 className='font-bold text-4xl md:text-5xl text-center lg:text-left text-white'>Watch everywhere.</h1>
+                <h1 className='font-bold text-4xl md:text-5xl text-center lg:text-left text-white'>Everywhere, without worries.</h1>
                 <p className='text-2xl text-white text-center lg:text-left pt-5'>Enjoy your favorites movies and series trailers on your phone, tablet, and laptop without any hasle.</p>
             </div>
         </div>
