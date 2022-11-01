d9ba8c4fe6cded4836fe9f3ea34c1b4d4b6e4ff9
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-10-31 20:53:08 -0600

    Radically changed searchForm component, Add ability to query strings to the back-end

--- client/src/components/SearchForm.tsx
+++ client/src/components/SearchForm.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { FaSearch } from 'react-icons/fa'
+import React, { useEffect, useState } from 'react'
+import { FaSearch, FaTimes } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
 
 interface IState {
     search: {
@@ -8,29 +9,57 @@ interface IState {
 }
 
 const SearchForm: React.FC = () => {
+  const navigation = useNavigate()
   const [input, setInput] = useState<IState["search"]>({
     search: ""
   })
+  const params = new URLSearchParams(window.location.search)
+  const search = params.get('search')
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    if (search !== null) {
+      setInput({search: search})
+    }
+    else if (window.location.search === "") {
+      setInput({search: ""})
+    }
+  }, [window.location.search])
+
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {   
     setInput({
-        ...input,
-        [e.target.name]: e.target.value
+      ...input,
+      [e.target.name]: e.target.value
     })
   }
 
+  const deleteInput = () => {
+    setInput({search: ""})
+    window.location.search = "?search="
+  }
+
   return (
-    <div className='grid gap-3 md:gap-0 md:flex pt-2'>
-      <input
-        className='w-full md:w-full lg:w-1/2 bg-gray1 placeholder:text-gray3 focus:outline-none focus:bg-gray2 px-2 py-2 md:py-3 rounded md:rounded-none md:rounded-l transition-all'
-        type="text"
-        placeholder="Name of movie or series.."
-        value={input.search}
-        name="search"
-        onChange={handleChange}
-      ></input>
-      <button className='flex py-2 md:py-0 bg-col1 hover:bg-col2 md:hover:bg-col2 md:bg-col1 rounded md:rounded-none md:rounded-r justify-center text-white text-white items-center md:px-3 transition-all'><FaSearch /></button>
-    </div>
+    <form id='searchForm' name='searchForm' className='grid gap-3 md:gap-0 md:flex pt-2 items-center'>
+      <div className="flex w-full lg:w-1/2 lg:max-w-lg bg-gray1 focus:outline-none rounded md:rounded-none md:rounded-l transition-all">
+        <input
+          className='w-full bg-gray1 placeholder:text-neutral-300 focus:outline-none px-2 py-2 md:py-3 rounded md:rounded-none md:rounded-l'
+          type="text"
+          placeholder="Name of movie or series.."
+          value={input.search}
+          id="search"
+          name="search"
+          onChange={handleChange}
+        ></input>
+        <div className={`${input.search.length === 0 ? "hidden": "grid"} bg-gray1 h-full py-4 px-2`}>
+          <FaTimes className='cursor-pointer hover:text-gray-300'
+          onClick={() => deleteInput()}
+          />
+        </div>
+      </div>
+      <button type={'submit'}
+      className='flex cursor-pointer py-2 md:py-4 bg-col1 hover:bg-col2 md:hover:bg-col2 md:bg-col1 rounded md:rounded-none md:rounded-r justify-center text-white items-center md:px-3 transition-all'><FaSearch />
+      </button>
+    </form>
   )
 }
 
