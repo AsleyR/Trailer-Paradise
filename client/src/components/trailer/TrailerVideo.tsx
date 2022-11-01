c17f3a8b21be3c65f32877929e08c9b7863b7d29
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-10-31 21:01:46 -0600

    Set autoplay option of the embedded YouTube video to ON

--- client/src/components/trailer/TrailerVideo.tsx
+++ client/src/components/trailer/TrailerVideo.tsx
@@ -10,7 +10,7 @@ const TrailerVideo = ({videoUrl}: IProps['trailer']) => {
   return (
     <div className='relative pb-[56.25%] h-0 transition-all'>
       <iframe
-      src={`${videoUrl}`} title="YouTube video player" 
+      src={`${videoUrl}?autoplay=1&start=0`} title="YouTube video player" 
       frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
       allowFullScreen className='absolute outline-0 focus:outline-0 focus-visible:outline-none top-0 left-0 w-[100%] h-[100%] rounded-none md:rounded justify-self-center transition-all'>
       </iframe>
