935318f2ec819b9c9528d99212a38ca428dad6a4
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-01 19:56:39 -0600

    Change text display when 404 error happens

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
     <div className='max-w-full h-screen bg-dark2'>
       <div className='grid grid-cols-1 justify-items-center px-0 md:px-10 lg:px-10 pt-56'>
           <h1 className='px-2 lg:px-0 font-bold text-col1 text-6xl sm:text-5xl md:text-7xl text-center'>Lost your way?</h1>
-          <h2 className='px-10 lg:px-80 my-5 text-lg text-white sm:text-lg md:text-2xl font-thin text-center'>Sorry, we can't find that page. You'll find lots to explore on the home page.</h2>
+          <h2 className='px-10 lg:px-80 my-5 text-lg text-white sm:text-lg md:text-2xl font-thin text-center'>Sorry, we can't find that page. Perhaps you may want to return to the homepage.</h2>
           <div className='bg-white hover:bg-gray-300 text-base lg:text-lg px-5 lg:px-8 py-1 lg:py-2 rounded cursor-pointer transition-all'>
             <Link className='text-gray-800 font-bold' to={"/"}>Home Page</Link>
           </div>
