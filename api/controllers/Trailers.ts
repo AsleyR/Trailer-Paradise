1308e316bba47e30f185b2466cc3cb1b1317f352
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-04 16:30:29 -0600

    Add types to req and res variables used in the routes, link accepted CORS connections to a single .env file

--- api/controllers/Trailers.ts
+++ api/controllers/Trailers.ts
@@ -1,34 +1,36 @@
+import { Request, Response} from 'express'
 import dotenv from 'dotenv'
 const Trailer = require("../models/Trailer")
 
 // Config .env variables
 dotenv.config()
+const FRONTEND_CONNECTION = process.env.FRONTEND_URL_CONNECTION
 
-exports.findTrailers = async (req: any, res: any) => {
+exports.findTrailers = async (req: Request, res: Response) => {
     // const trailers = await Trailer.find()
-    const trailers = await Trailer.aggregate([{$sample: {size: 99}}]) // Experimenting with returning the trailers objects in random order
-    res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`)
+    const trailers = await Trailer.aggregate([{$sample: {size: 99}}]) // Experimenting with returning trailers objects in a semi-random order
+    res.header("Access-Control-Allow-Origin", `${FRONTEND_CONNECTION}`)
     res.status(200).send(trailers)
 }
 
-exports.createTrailer = async (req: any, res: any) => {
+exports.createTrailer = async (req: Request, res: Response) => {
     const trailer = new Trailer(req.body)
     await trailer.save()
-    res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`)
+    res.header("Access-Control-Allow-Origin", `${FRONTEND_CONNECTION}`)
     res.status(200).send(trailer)
 }
 
-exports.findTrailerById = async (req: any, res: any) => {
+exports.findTrailerById = async (req: Request, res: Response) => {
     const requestId = req.params.id
 
     try {
         if (requestId === "random") {
             const trailer = await Trailer.aggregate([{$sample: {size: 1}}])
-            res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`)
+            res.header("Access-Control-Allow-Origin", `${FRONTEND_CONNECTION}`)
             res.status(200).send(trailer)
         } else {
             const trailer = await Trailer.findById(req.params.id)
-            res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`)
+            res.header("Access-Control-Allow-Origin", `${FRONTEND_CONNECTION}`)
             res.status(200).send(trailer)
         }
     } catch (err) {
@@ -36,12 +38,12 @@ exports.findTrailerById = async (req: any, res: any) => {
     }
 }
 
-exports.findTrailerByName = async (req: any, res: any) => {
+exports.findTrailerByName = async (req: Request, res: Response) => {
     const requestName = req.params.name
 
     try {
         const trailer = await Trailer.find( { title: { $regex: `${requestName}`, $options: 'ims' } } )
-        res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`)
+        res.header("Access-Control-Allow-Origin", `${FRONTEND_CONNECTION}`)
         res.status(200).send(trailer)
     } catch (err) {
         res.status(404).send({result: "Trailer object was not found.", error: err})
