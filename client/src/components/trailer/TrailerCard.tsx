f6fe1469f9264b67b5892ea56290c335f1bf647f
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-10 14:11:33 -0600

    Add alt to trailerCards cover img

--- client/src/components/trailer/TrailerCard.tsx
+++ client/src/components/trailer/TrailerCard.tsx
@@ -16,7 +16,7 @@ const TrailerCard = ({ trailer }: IProps) => {
   return (
     <>
         <li className={`w-full h-full sm:w-full bg-gray grid shadow-lg hover:scale-105 transition-all duration-300 cursor-pointer`} onClick={() => {navigation(`/trailer/${trailer._id}`)}}>
-            <img className='mx-auto w-full h-full object-fill rounded' src={trailer.coverUrl}/>
+            <img className='mx-auto w-full h-full object-fill rounded' src={trailer.coverUrl} alt={'Trailer cover'}/>
             <div className='relative'>
                 <span className='bg-gradient-to-b to-gray2 from-transparent w-full absolute bottom-0 text-base md:text-base lg:text-sm font-bold lg:font-normal text-center text-white pt-10 pb-1 px-5 md:px-1 rounded'>{trailer.title}</span>
             </div>
