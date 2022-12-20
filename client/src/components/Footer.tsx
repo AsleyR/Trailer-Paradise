e6f58945aab194e96c9523a9d3f4740ad74a8de3
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-19 18:52:24 -0600

    Fix broken link, fix mobile view of footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -8,11 +8,12 @@ const Footer = () => {
       <div className='flex justify-center px-5'>
         <div className='text-bg2'>
             <span>Watch the lastest trailers today!</span>
-            <Link className='bg-col1 text-white rounded-full hover:bg-col2 ml-3 px-5 py-1' to={'/explore'}>Explore!</Link>
+            <Link className='bg-col1 text-white rounded-full hover:bg-col2 ml-3 px-5 py-1' to={'/explore?search='}>Explore!</Link>
         </div>
       </div>
-      <div className=''>
-          <h1 className='text-bg2'>Licensed under <a className='hover:underline' href='https://github.com/AsleyR/Trailer-Paradise/blob/main/LICENSE' target={'_blank'}>GPL-3.0-or-later</a>. All Rights Reserved. </h1>
+      <div className='grid sm:grid-none flex-none sm:flex gap-0 sm:gap-1 text-bg2 text-center justify-center'>
+          <h1 className='max-w-xs sm:max-w-full'>Licensed under <a className='underline' href='https://github.com/AsleyR/Trailer-Paradise/blob/main/LICENSE' target={'_blank'}>GPL-3.0-or-later</a>.</h1>
+          <h1 className='max-w-xs sm:max-w-full'>All Rights Reserved. </h1>
       </div>
     </footer>
   )
