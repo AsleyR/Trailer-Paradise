1b33ff7a8c81be7c172cea722b57e307cf0f9ab6
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-12-19 18:53:01 -0600

    Delete some text inside ConnectionError component

--- client/src/components/error/ConnectionError.tsx
+++ client/src/components/error/ConnectionError.tsx
@@ -6,7 +6,6 @@ const ConnectionError = () => {
       <div className="grid">
         <p className='text-lg'>Oops! It seems there was an error while trying to connect to the server.</p>
       </div>
-      <p className='text-base font-bold'>(Server Error - 500)</p>
     </div>
   )
 }
